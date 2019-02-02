74fead5b1ca9fefb708c71d256162b9bd21d311d
imverka <mpsv1992@gmail.com>
2019-02-02 12:21:49 -0500

    URI changed, Online mLab

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
     //local Mongo DB deployment
-    "URI":"mongodb://localhost/demo"
+    //"URI":"mongodb://localhost/demo"
+    "URI":"mongodb://maninder:a123456@ds056288.mlab.com:56288/comp308-w2019-lesson4a"
 }
