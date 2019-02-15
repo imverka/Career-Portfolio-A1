f94ae1eccc8bbf6813b4af5650c989b342a779e5
imverka <mpsv1992@gmail.com>
2019-02-15 02:13:27 -0500

    edit functionality added

--- routes/contact.js
+++ routes/contact.js
@@ -52,5 +52,48 @@ router.post('/add', (req, res, next) =>{
     })
 });
 
+//GET request - display the edit page
+
+router.get('/edit/:id', (req,res,next) => {
+    let id = req.params.id;
+
+    contactModel.findById(id, (err, contactObject) => {
+        if(err){
+            console.log(err);
+            res.end(err);
+        }
+        else{
+            //show the edit view
+            res.render('contacts/edit', {
+                title: 'Edit Contact',
+                contact: contactObject
+            })
+        }
+    });
+
+});
+
+//POST request to update the database with data from edit page
+router.post('/edit/:id', (req, res, next) => {
+    let id = req.params.id;
+    let updatedContact = contactModel({
+        "_id": id,
+        "firstName": req.body.firstName,
+        "lastName": req.body.lastName,
+        "age": req.body.age
+    });
+
+    contactModel.update({_id: id}, updatedContact, (err) => {
+        if(err){
+            console.log(err);
+            res.end(err);
+        }
+        else{
+            //refresh the contact list
+            res.redirect('/contact-list');
+        }
+    });
+
+});
 
 module.exports = router;
