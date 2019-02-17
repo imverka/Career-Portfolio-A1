e877b54579c6797d7fbf39e3f233a501e0d6d843
imverka <mpsv1992@gmail.com>
2019-02-16 19:09:35 -0500

    errors v4

--- server/config/app.js
+++ server/config/app.js
@@ -4,19 +4,6 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
-// //database setup
-// let mongoose = require("mongoose");
-// let DB = require("./db");
-
-// //pont Mongoose to DB URI
-// mongoose.connect(DB.URI);
-
-// let mongoDB = mongoose.connection;
-// mongoDB.on('error', console.error.bind(console, 'Connection Error:'));
-// mongoDB.once('open', () => {
-//   console.log("Connected to MongoDB....");
-// })
-
 //route setup
 let indexRouter = require('../routes/index');
 let contactRouter = require('../routes/contact');
